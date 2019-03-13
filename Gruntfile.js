309f2d5f19f32f592a41a340c954b4e357f106ed
wangding <408542507@qq.com>
2019-03-13 19:23:57 +0800

    grunt-demo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,28 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    sprite: {
-      all: {
-        src: 'images/*.png',
-        dest: 'dist/spritesheet.png',
-        destCss: 'dist/sprites.css'
-                                                    
-      }
+    run: {
+      api: {
+        options: { wait: false  },
+        args: ['./app.js']
           
-    }
-      
+      }
+               
+    },
+  mochacli: {
+    options: {
+      reporter: 'spec',
+      bail: true
+          
+    },
+    all: ['test/*.js']
+                           
+  }
+    
   });
 
-  grunt.loadNpmTasks('grunt-spritesmith');
+  grunt.loadNpmTasks('grunt-run')
+  grunt.loadNpmTasks('grunt-mocha-cli')
 
-  grunt.registerTask('default', ['sprite']);
+  grunt.registerTask('default', ['run', 'mochacli', 'stop:api']);
 
 };
