a5dec255448486f9eee08e2b61fe697b0e797f12
wangding <408542507@qq.com>
2019-03-13 17:20:46 +0800

    grunt-demo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,44 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    csslint: {
+    mochacli: {
       options: {
-        csslintrc: '.csslintrc'
+                reporter: 'spec',
+          bail: true
           
       },
-      src: ['*.css']
+        all: ['test/*.js']
+                         
+    },
+    mocha_istanbul: {
+    coverage: {
+            src: 'test'
+                  
+    }
+        
+    },
+    istanbul_check_coverage: {
+    default: {
+    options: {
+              coverageFolder: 'coverage*',
+              check: {
+                          lines: 90,
+                                      statements: 90
+                                                
+              }
+                      
+    }
+          
+    }
         
     }
-                
+      
   });
 
-grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-mocha-cli');
+      grunt.loadNpmTasks('grunt-mocha-istanbul');
 
-grunt.registerTask('default', ['csslint']);
+        grunt.registerTask('default', ['mochacli']);   
+          grunt.registerTask('cover', ['mocha_istanbul']);
+            grunt.registerTask('check-cover', ['istanbul_check_coverage']);
 
 };
